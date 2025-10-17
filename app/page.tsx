819ef73076a8dda556a7f7d544989a0972e1d772
Cédrick Alexandre <alexandre.cedrick@gmail.com>
2025-10-17 13:11:10 +0200

    First commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,151 @@
-import Image from "next/image";
+'use client';
+
+import { useState } from "react";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectLabel,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+import { Button } from "@/components/ui/button";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+
 
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const exercices = [
+    {
+      bodyPart: "Pectoraux",
+      excercices: [
+        "Banc",
+        "Banc incliné",
+        "Fly machine",
+      ]
+    },
+    {
+      bodyPart: "Dos",
+      excercices: [
+        "Tirage horizontal",
+        "Tirage vertical",
+        "Tirage assité un bras",
+      ]
+    },
+    {
+      bodyPart: "Épaules",
+      excercices: [
+        "Shoulder press machine",
+        "Oiseau debout avec poids/haltères",
+      ]
+    },
+    {
+      bodyPart: "Biceps",
+      excercices: [
+        "Curl biceps assis à la machine",
+        "Curl haltère incliné",
+        "Curl biceps sur mur",
+      ]
+    },
+  ]
+
+  const [seance, setSeance] = useState<any[]>([]);
+  
+  function handleSubmit(event: any) {
+    event.preventDefault();
+    const form = event.target;
+    const formData = new FormData(form);
+    const data = Object.fromEntries(formData.entries());
+    console.log(data);
+
+    setSeance([...seance, data]);
+    form.reset();
+  }
+
+  return (
+    <>
+      <form 
+        className="flex flex-col justify-center py-2 max-w-2xl m-auto"
+        onSubmit={handleSubmit}
+      >
+        <div className="grid w-full items-center gap-3 mb-4">
+          <Label htmlFor="exercice">Exercice</Label>
+          <Select name="exercice">
+            <SelectTrigger className="w-full">
+              <SelectValue placeholder="Select an exercice" />
+            </SelectTrigger>
+            <SelectContent>
+              {
+                exercices.map((exercice, index) => (
+                  <SelectGroup key={exercice.bodyPart}>
+                    <SelectLabel>{exercice.bodyPart}</SelectLabel>
+                    {exercice.excercices.map((name) => (
+                      <SelectItem key={name} value={name}>{name}</SelectItem>
+                    ))}
+                  </SelectGroup>
+                ))
+              }
+            </SelectContent>
+          </Select>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+
+        <div className="flex justify-between w-full gap-6">
+          <div className="grid w-full items-center gap-3 mb-4">
+            <Label htmlFor="weight">Poids (kg)</Label>
+            <Input type="number" name="weight" min="2.5" step="1.25"/>
+          </div>
+          <div className="grid w-full items-center gap-3 mb-4">
+            <Label htmlFor="serie">Série</Label>
+            <Input type="number" name="serie" min="1"/>
+          </div>
+
+          <div className="grid w-full items-center gap-3 mb-4">
+            <Label htmlFor="repetition">Répétitions</Label>
+            <Input type="number" name="repetition" min="1"/>
+          </div>
+        </div>
+
+        <div className="flex mt-4">
+          <Button type="submit">Ajouter</Button>
+        </div>
+      </form>
+
+      {
+        seance.length > 0 &&
+        <Table className="flex flex-col justify-center py-2 max-w-2xl m-auto">
+          <TableCaption>Séance</TableCaption>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-200 text-left">Machine/Exercice</TableHead>
+              <TableHead className="w-200 text-left">Poids</TableHead>
+              <TableHead className="w-200 text-left">Série</TableHead>
+              <TableHead className="w-200 text-left">Répétition</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            { seance.map((item, index) => (
+              <TableRow key={index}>
+                <TableCell className="w-200 text-left">{item.exercice}</TableCell>
+                <TableCell className="w-200 text-left">{item.weight} kg</TableCell>
+                <TableCell className="w-200 text-left">{item.serie}</TableCell>
+                <TableCell className="w-200 text-left">{item.repetition}</TableCell>
+              </TableRow>
+            )) }
+          </TableBody>
+        </Table>
+      }
+      
+    </>
   );
 }
